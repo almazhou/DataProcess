162279e0c8ff7c6ed62fa46e8687da22bff8461f
almazhou <zx_new@126.com>
2013-09-07 16:46:06 +0800

    AlmaZhou -Show index page using Spring MVC

---
+++ src/main/java/com/springapp/mvc/FinanceController.java
@@ -0,0 +1,17 @@
+package com.springapp.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class FinanceController {
+    @RequestMapping(value = "/" ,method = RequestMethod.GET)
+    public ModelAndView showIndex() {
+
+        ModelAndView index = new ModelAndView("index");
+        index.getModel().put("message","ThoughtWorks index page");
+        return index;
+    }
+}
---
+++ src/test/java/com/springapp/mvc/FinanceControllerTest.java
@@ -0,0 +1,22 @@
+package com.springapp.mvc;
+
+import org.junit.Test;
+import org.springframework.web.servlet.ModelAndView;
+
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class FinanceControllerTest {
+    @Test
+    public void should_return_correct_model_and_view() throws Exception {
+        FinanceController financeController = new FinanceController();
+
+        ModelAndView modelAndView = financeController.showIndex();
+
+        assertThat(modelAndView,notNullValue());
+        assertThat(modelAndView.getViewName(),is("index"));
+    }
+
+
+}
